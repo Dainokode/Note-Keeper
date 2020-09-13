5d872ba918c5e88747914c137a19b66cac12d25e
Dainokode <antoniodainok@gmail.com>
2020-09-13 10:34:21 +0200

    each note is wrapped by a container div

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Note = () => {
   return (
-    <div className="note">
-      <h2>Note title</h2>
-      <p>Note content</p>
+    <div className="container">
+      <div className="note">
+        <h2>Note title</h2>
+        <p>Note content</p>
+      </div>
     </div>
   );
 };
